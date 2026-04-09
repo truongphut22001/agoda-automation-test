0e7346fa329c7a0d794ae89d92970a2edb15c97c
truongphut22001 <truongphut22001@gmail.com>
2026-04-09 22:27:22 +0700

    feat: implement BasePage with common driver utilities

---
+++ automation/src/test/java/pages/BasePage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    protected WebElement waitForVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void click(By locator) {
+        waitForClickable(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        waitForVisible(locator).clear();
+        waitForVisible(locator).sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisible(locator).getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        return waitForVisible(locator).isDisplayed();
+    }
+}
